078860553138bd89cc0ed38ec53b371e8a35e1b9
Deen Adzemovic <deen.adzemovic@hotmail.com>
2015-01-01 17:32:03 -0500

    Removal of recursive function
    From recursive function to while loop in order to minimize the
    possibility of a stack overflow. Other correcetions based on reddit
    feedback.

--- main.cpp
+++ main.cpp
@@ -5,87 +5,71 @@
 #include <limits>       /* numeric_limits */
 #include <stdlib.h>     /* exit, EXIT_SUCCESS */
 
-using namespace std;
-
-// Global variables
-int randomInt, userMove;
-char playAgain;
-const string moves[] = {"rock", "paper", "scissors"};
-
-// Empties the input stream
-void clearStream()
-{
-    cin.clear();
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-    return;
-}
-
-void run(int playerWins = 0, int compWins = 0)
-{
-    cout << "Enter 1 to play Rock, 2 to play Paper, and 3 to play Scissors!: ";
-    cin >> userMove;
-
-    while(!(cin and userMove >= 1 and userMove <= 3))
-    {
-        cout << "Unknown command! Please try that again..." << endl;
-        clearStream();
-        cout << "Enter 1 to play Rock, 2 to play Paper, and 3 to play Scissors!: ";
-        cin >> userMove;
-    }
-
-    // Create random integer in range [1,3] to simulate the computer selecting a move
-    randomInt = rand() % 3 + 1;
-
-    if(userMove == randomInt)
-        cout << "Tie game!\nBoth you and the computer chose " << moves[randomInt - 1] << endl;
-    else
-    {
-        if(userMove - randomInt == 1 or userMove - randomInt == -2)
-        {
-            cout << "Congratulations, you won!" << endl;
-            playerWins++;
-        } else
-        {
-            cout << "Sorry, better luck next time!" << endl;
-            compWins++;
-        }
-
-        cout << "You chose " << moves[userMove - 1] << " and the computer chose " << moves[randomInt - 1] << endl;
-    }
-
-    cout << "(Player: " << playerWins << " | Computer: " << compWins << ")" << endl;
-
-    clearStream();
-    cout << "Play again? [y/n]: ";
-    cin >> playAgain;
-    
-    while(!(cin and playAgain == 'y' or playAgain == 'n'))
-    {
-        cout << "Unknown command! Please try that again..." << endl;
-        clearStream();        
-        cout << "Play again? [y/n]: ";
-        cin >> playAgain;      
-    }
-
-    if(playAgain == 'y')
-    {
-        clearStream();
-        run(playerWins, compWins);
-    }
-    else
-        exit(EXIT_SUCCESS);
-
-    return;
-}
-
 int main()
 {
+    bool playing = true;
+    std::string playAgain, line;
+    int playerWins = 0, compWins = 0, compMove, userMove;
+    const std::string moves[] = {"rock", "paper", "scissors"};
+
     // Initialize rng
     srand(time(NULL));
 
-    cout << "Welcome. Please press the enter key to begin playing Rock Paper Scissors SHOOT!" << endl << "NOTE: Press CTRL+C at any time to terminate the game!";
-    clearStream(); // Pauses the program until the enter key is pressed and simultaneously clears the input stream if the user input anything before hitting enter
-    run();
+    std::cout << "Welcome. Please press the enter key to begin playing Rock Paper Scissors!";
+    // Pauses the program
+    getline(std::cin, line);
+
+    while(playing)
+    {
+        std::cout << "Enter 1 to play Rock, 2 to play Paper, and 3 to play Scissors!: ";
+        std::cin >> userMove;
+
+        while(!(std::cin and userMove >= 1 and userMove <= 3))
+        {
+            std::cout << "Unknown command! Please try that again..." << std::endl;
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cout << "Enter 1 to play Rock, 2 to play Paper, and 3 to play Scissors!: ";
+            std::cin >> userMove;
+        }
+
+        // Create random integer in range [1,3] to simulate the computer selecting a move
+        compMove = rand() % 3 + 1;
+
+        if(userMove == compMove)
+            std::cout << "Tie game!\nBoth you and the computer chose " << moves[compMove - 1] << std::endl;
+        else
+        {
+            if(userMove - compMove == 1 or userMove - compMove == -2)
+            {
+                std::cout << "Congratulations, you won!" << std::endl;
+                playerWins++;
+            } else
+            {
+                std::cout << "Sorry, better luck next time!" << std::endl;
+                compWins++;
+            }
+
+            std::cout << "You chose " << moves[userMove - 1] << " and the computer chose " << moves[compMove - 1] << std::endl;
+        }
+
+        std::cout << "(Player: " << playerWins << " | Computer: " << compWins << ")" << std::endl;
+
+
+        std::cout << "Play again? [y/n]: ";
+        std::cin >> playAgain;
+        
+        while(!(std::cin and playAgain == "y" or playAgain == "n"))
+        {
+            std::cout << "Unknown command! Please try that again..." << std::endl;
+
+            std::cout << "Play again? [y/n]: ";
+            std::cin >> playAgain;      
+        }
+
+        if(playAgain != "y")
+            playing = false;
+    }
 
     return 0;
 }
